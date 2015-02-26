069ea861f17f1ef5b4d5c19587c0b6edd8d5c911
Calvin Nolan <Calvinnolan@gmail.com>
2015-02-26 14:46:45 +0000

    Added second screen that print the returned request

--- gen/com/group17/smart_gh/R.java
+++ gen/com/group17/smart_gh/R.java
@@ -1757,11 +1757,13 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030014;
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_display_message=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
-        public static final int main=0x7f0d0000;
+        public static final int display_message=0x7f0d0000;
+        public static final int main=0x7f0d0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1826,6 +1828,7 @@ containing a value of this type.
         public static final int from=0x7f0a0015;
         public static final int hello_world=0x7f0a0011;
         public static final int search=0x7f0a0016;
+        public static final int title_activity_display_message=0x7f0a0017;
         public static final int to=0x7f0a0014;
     }
     public static final class style {
